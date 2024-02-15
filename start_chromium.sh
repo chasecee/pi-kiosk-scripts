79f5fff42b9493bd7c86f2f7de085b21a02c9557
Chase Christensen <chaserchristensen@gmail.com>
2024-02-15 10:39:00 -0700

    chromium mods

--- start_chromium.sh
+++ start_chromium.sh
@@ -6,5 +6,5 @@ xset s off >> $LOGFILE 2>&1
 xset -dpms >> $LOGFILE 2>&1
 xset s noblank >> $LOGFILE 2>&1
 unclutter -idle 0.1 -root & >> $LOGFILE 2>&1
-chromium-browser --kiosk --noerrdialogs --no-restore --disable-translate --disable-infobars --disable-features=TranslateUI --incognito --disable-gpu "https://pi-dashboard-one.vercel.app/" >> $LOGFILE 2>&1
+chromium-browser --kiosk --noerrdialogs --no-restore --disable-translate --disable-infobars --disable-features=TranslateUI --incognito --disable-gpu --disable-screensaver --autoplay-policy=no-user-gesture-required --overscroll-history-navigation=0 --no-first-run --no-default-browser-check "https://pi-dashboard-one.vercel.app/" >> $LOGFILE 2>&1
 echo "$(date) - Chromium has started" >> $LOGFILE
