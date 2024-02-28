627224776371b9c2e06fa2d7b5c782b79e847ae8
Chase Christensen <chaserchristensen@gmail.com>
2024-02-28 15:25:19 -0700

    🔧 (start_chromium.sh): Update Chromium startup script to disable energy saving settings, screen blanking, and hide mouse when idle for better kiosk mode experience   🚀 (start_chromium.sh): Start Chromium in kiosk mode with additional flags to address errors and improve performance   📝 (start_chromium.sh): Update log file maintenance logic for size management for better file management efficiency

--- start_chromium.sh
+++ start_chromium.sh
@@ -2,14 +2,21 @@
 export DISPLAY=:0
 LOGFILE="/home/pi/pi-kiosk-scripts/start_chromium.log"
 echo "$(date) - Starting Chromium" >> $LOGFILE
+
+# Disabling energy saving settings and screen blanking
 xset s off >> $LOGFILE 2>&1
 xset -dpms >> $LOGFILE 2>&1
 xset s noblank >> $LOGFILE 2>&1
+
+# Hiding the mouse when not in use
 unclutter -idle 0.1 -root & >> $LOGFILE 2>&1
-chromium-browser --kiosk --noerrdialogs --no-restore --disable-translate --disable-infobars --disable-features=TranslateUI --incognito --disable-gpu --disable-screensaver --autoplay-policy=no-user-gesture-required --overscroll-history-navigation=0 --no-first-run --no-default-browser-check --renderer-process-limit=50 "https://pi-dashboard-one.vercel.app/" >> $LOGFILE 2>&1
+
+# Starting Chromium in kiosk mode with additional flags to address errors
+chromium-browser --kiosk --noerrdialogs --disable-session-crashed-bubble --disable-infobars --incognito --disable-translate --disable-features=TranslateUI --autoplay-policy=no-user-gesture-required --overscroll-history-navigation=0 --no-first-run --no-default-browser-check --disable-background-timer-throttling --disable-breakpad --check-for-update-interval=31536000 --disable-component-update --enable-features=OverlayScrollbar --force-device-scale-factor=1 "https://pi-dashboard-one.vercel.app/" >> $LOGFILE 2>&1
+
 echo "--------HEYY------------- $(date) - Chromium has started" >> $LOGFILE
 
-# Log trimming
+# Log file maintenance for size management
 MAX_SIZE=50000 # Maximum file size in bytes (50KB in this example)
 if [ $(stat -c%s "$LOGFILE") -ge $MAX_SIZE ]; then
     tail -n 1000 "$LOGFILE" > "$LOGFILE.tmp" && mv "$LOGFILE.tmp" "$LOGFILE"
